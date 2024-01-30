c63649f5162278a446d97f5dcbd999850f33eb25
Velicko00 <145012953+andjelabel@users.noreply.github.com>
2024-01-30 13:50:42 +0100

    properties home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import "./home.css";
 
 const Home = () => {
@@ -7,6 +10,13 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+      </div>
     </div>
   );
 };
