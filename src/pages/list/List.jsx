bedf516555b2f0a3b07b90a4d19d872486a9daf9
Velicko00 <145012953+andjelabel@users.noreply.github.com>
2024-01-30 18:08:48 +0100

    search items

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   const location = useLocation();
@@ -84,6 +85,9 @@ const List = () => {
               </div>
             </div>
             <button>Search</button>
+          </div>
+          <div className="listResult">
+            <SearchItem />
           </div>
         </div>
       </div>
