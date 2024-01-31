b2e3a82a94edcf27843802f0791f1455c44d1848
Velicko00 <stefan.velickovic00@gmail.com>
2024-01-31 02:02:29 +0100

    slider slike

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -5,42 +5,83 @@ import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+  faCircleXmark,
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
-
+import { useState } from "react";
 
 const Hotel = () => {
-
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
 
   const photos = [
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/205340676.jpg?k=298ad78cc1c5e6498b885231c06cbb84170f0ba715aac2af632bfb9e3a1b73e0&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/209687994.jpg?k=6c47403c211dd6b70009a1496e4ae95751619956914fd80f9ea636bd43bd9fbb&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/209688530.jpg?k=b5e2f1e35ebac6a6b7750b584334aceb51e7c26431a5a4faac63ce206f8c3faa&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/205358326.jpg?k=466e33a3ed4abdb305e682386fdbe71afb7f4008420979437963e00c24831c05&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/205343006.jpg?k=9375795d72cd6940e8c4f9f358336119c949bd3c673a9e3e176de0dc5166f6e5&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/209688795.jpg?k=41a1cc7b7d6fa6b68ad3a8b8d4d931f2bba46021169a6b85f4be2957346c2f77&o=&hp=1",
     },
   ];
 
- 
+  const handleOpen = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+  };
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newSlideNumber)
+  };
+
 
   return (
     <div>
       <Navbar />
       <Header type="list" />
       <div className="hotelContainer">
-        
+        {open && (
+          <div className="slider">
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="close"
+              onClick={() => setOpen(false)}
+            />
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className="arrow"
+              onClick={() => handleMove("l")}
+            />
+            <div className="sliderWrapper">
+              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+            </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className="arrow"
+              onClick={() => handleMove("r")}
+            />
+          </div>
+        )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
           <h1 className="hotelTitle">Hotel Ambasador</h1>
@@ -55,10 +96,10 @@ const Hotel = () => {
             Book a stay over $214 at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
-          {photos.map((photo, i) => (
+            {photos.map((photo, i) => (
               <div className="hotelImgWrapper" key={i}>
                 <img
-                
+                  onClick={() => handleOpen(i)}
                   src={photo.src}
                   alt=""
                   className="hotelImg"
@@ -70,10 +111,10 @@ const Hotel = () => {
             <div className="hotelDetailsTexts">
               <h1 className="hotelTitle">Stay in the heart of City</h1>
               <p className="hotelDesc">
-              Set in Niš, 200 metres from King Milan Square, Ambasador Hotel offers accommodation with free bikes, 
-              free private parking, a fitness centre and a shared lounge. With a terrace, the 5-star hotel has 
-              air-conditioned rooms with free WiFi, each with a private bathroom. The accommodation features room service, 
-              a 24-hour front desk and currency exchange for guests.
+                Set in Niš, 200 metres from King Milan Square, Ambasador Hotel offers accommodation with free bikes,
+                free private parking, a fitness centre and a shared lounge. With a terrace, the 5-star hotel has
+                air-conditioned rooms with free WiFi, each with a private bathroom. The accommodation features room service,
+                a 24-hour front desk and currency exchange for guests.
               </p>
             </div>
             <div className="hotelDetailsPrice">
